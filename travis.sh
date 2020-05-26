468fbf8304cf7cb44c9df369237420246db0d56e
Nathan Robinson <nrobinson2000@me.com>
2020-05-26 18:38:42 -0400

    fix ci at 18:38 of 2020-05-26

--- travis.sh
+++ travis.sh
@@ -2,7 +2,7 @@ export PATH="$PATH:$PWD"
 
 if [ "$(uname -s)" == "Linux" ]; then
 sudo apt update
-sudo apt install python3-pip libarchive-zip-perl
+sudo apt install python3-pip libarchive-zip-perl libc6-i386
 fi
 
 pip3 install -r requirements.txt
