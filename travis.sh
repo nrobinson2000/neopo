8f05ad1ad8341dca529202f076a264daf6170d79
JuniorCode <jacob.alan.robinson10@gmail.com>
2020-05-26 22:05:55 -0400

    shebang?

--- travis.sh
+++ travis.sh
@@ -1,6 +1,11 @@
+#!/bin/bash
+
+# neopo: A lightweight solution for local Particle development.
+# Copyright (c) 2020 Nathan Robinson.
+
 export PATH="$PATH:$PWD/bin"
 
-if [ "$(uname -s)" == "Linux" ]; then
+if [ "$(uname)" == "Linux" ]; then
     sudo apt update
     sudo apt install libarchive-zip-perl libc6-i386
 fi
