a018fe8ef17ff73c8d374337008602a8650183bc
Nathan Robinson <nrobinson2000@me.com>
2020-05-27 13:39:04 -0400

    fix at 13:39 of 2020-05-27

--- test/target/argon/obj/src/user_module.o.d
+++
@@ -1,8 +0,0 @@
-/home/nrobinson/code/neopo/test/target/argon//obj/./src/user_module.o: \
- src/user_module.c \
- ../../../modules/shared/nRF52840/inc/user-part/user_module.inc \
- ../../../dynalib/inc/dynalib.h
-
-../../../modules/shared/nRF52840/inc/user-part/user_module.inc:
-
-../../../dynalib/inc/dynalib.h:
