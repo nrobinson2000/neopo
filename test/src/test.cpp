cb0770801ba39ee0a7bb45ac255379c4151c44b5
Nathan Robinson <nrobinson2000@me.com>
2020-05-27 13:38:42 -0400

    exit returncode if subprocess fails at 13:38 of 2020-05-27

---
+++ test/src/test.cpp
@@ -0,0 +1,30 @@
+/******************************************************/
+//       THIS IS A GENERATED FILE - DO NOT EDIT       //
+/******************************************************/
+
+#include "Particle.h"
+#line 1 "/home/nrobinson/code/neopo/test/src/test.ino"
+/*
+ * Project test
+ * Description:
+ * Author:
+ * Date:
+ */
+
+// setup() runs once, when the device is first turned on.
+void setup();
+void loop();
+#line 9 "/home/nrobinson/code/neopo/test/src/test.ino"
+void setup() {
+
+
+  //buggboi
+  // Put initialization like pinMode and begin functions here.
+
+}
+
+// loop() runs over and over again, as quickly as it can execute.
+void loop() {
+  // The core of your code will likely live here.
+
+}
---
+++ test/src/test.ino
@@ -0,0 +1,21 @@
+/*
+ * Project test
+ * Description:
+ * Author:
+ * Date:
+ */
+
+// setup() runs once, when the device is first turned on.
+void setup() {
+
+
+  //buggboi
+  // Put initialization like pinMode and begin functions here.
+
+}
+
+// loop() runs over and over again, as quickly as it can execute.
+void loop() {
+  // The core of your code will likely live here.
+
+}
