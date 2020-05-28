1706b662c3e904339667e393f552573666bdab06
Nathan Robinson <nrobinson2000@me.com>
2020-05-28 12:15:35 -0400

    improve installation script at 12:15 of 2020-05-28

--- install.sh
+++ install.sh
@@ -3,19 +3,14 @@
 # neopo: A lightweight solution for local Particle development.
 # Copyright (c) 2020 Nathan Robinson.
 
-case "$(uname)" in
-    Linux)
-        a="$HOME/.local/bin/neopo";;
-
-    Darwin)
-        a='/usr/local/bin/neopo';;
-
-    *)
-        >&2 echo "OS is not supported! Use Linux or macOS."
-
-        exit 1
+case "$(uname)" in Linux)
+echo "You may need to install the following dependencies:"
+echo -e "\tlibarchive-zip-perl libc6-i386"
+echo -e "\tsudo apt install libarchive-zip-perl libc6-i386" ;;
+Darwin) ;;
+*) >&2 echo "Your OS is not supported! Use Linux or macOS." ; exit 1
 esac
 
-echo "Downloading neopo into $a..."
-
-curl -Lo "$a" https://raw.githubusercontent.com/nrobinson2000/neopo/master/bin/neopo && chmod +x "$a" && "$a" install
+echo "Downloading installation script..."
+curl -LO "https://raw.githubusercontent.com/nrobinson2000/neopo/master/bin/install.py"
+python3 install.py && neopo install
