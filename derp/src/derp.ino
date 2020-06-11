54897c0921692752c3ae39bce1500c797408121c
Nathan Robinson <nrobinson2000@me.com>
2020-06-11 18:19:27 -0400

    catch errors better in script at 18:19 of 2020-06-11

---
+++ derp/src/derp.ino
@@ -0,0 +1,18 @@
+/*
+ * Project derp
+ * Description:
+ * Author:
+ * Date:
+ */
+
+// setup() runs once, when the device is first turned on.
+void setup() {
+  // Put initialization like pinMode and begin functions here.
+
+}
+
+// loop() runs over and over again, as quickly as it can execute.
+void loop() {
+  // The core of your code will likely live here.
+
+}
