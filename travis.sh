6fdf9f0e3df9cbf9ca4c8a7727252aa3f82e002d
Nathan Robinson <nrobinson2000@me.com>
2020-08-22 00:28:06 -0400

    travis fix at 00:28 of 2020-08-22

--- travis.sh
+++ travis.sh
@@ -3,7 +3,7 @@
 # neopo: A lightweight solution for local Particle development.
 # Copyright (c) 2020 Nathan Robinson.
 
-export PATH="$PATH:$PWD/bin"
+sudo cp bin/neopo.py /usr/local/bin/neopo
 
 if [ "$(uname)" == 'Linux' ]; then
     sudo apt update
