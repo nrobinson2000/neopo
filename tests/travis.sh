e4dcc36a7301ec97743e588ed42276ad5ff859dc
Nathan Robinson <nrobinson2000@me.com>
2020-10-30 16:15:51 -0400

    fix linux travis at 16:15 of 2020-10-30

--- tests/travis.sh
+++ tests/travis.sh
@@ -7,5 +7,5 @@ sudo cp neopo/neopo.py /usr/local/bin/neopo
 
 if [ "$(uname)" == 'Linux' ]; then
     sudo apt update
-    sudo apt install libarchive-zip-perl libc6-i386
+    sudo apt install libarchive-zip-perl libc6-i386 python3-pip python3-setuptools
 fi
