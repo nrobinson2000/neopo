7f2b5a607505d552050376dce457fa6dc475af2a
Nathan Robinson <nrobinson2000@me.com>
2020-11-11 10:26:24 -0500

    github actions at 10:26 of 2020-11-11

--- tests/github-neopo.py
+++ tests/github-neopo.py
@@ -1,4 +1,5 @@
 #Attempt to use neopo as python module to configure and build project
 import neopo
-neopo.configure("argon", "2.0.0-rc.3", "test")
-neopo.build("test")
+neopo.create("testpy")
+neopo.configure("argon", "2.0.0-rc.3", "testpy")
+neopo.build("testpy")
