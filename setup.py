93193946441515491d6ccf34d57d8f4a5c1bbed0
Nathan Robinson <nrobinson2000@me.com>
2020-11-11 01:07:52 -0500

    [skip-ci] more python experimentation at 01:07 of 2020-11-11

--- setup.py
+++ setup.py
@@ -8,11 +8,10 @@ VERSION = "%s.%s" % (count, commit)
 # print(VERSION)
 
 share_files = [
-('usr/share/bash-completion/completions', ['neopo/neopo-completion'])
-]
+   ('usr/share/bash-completion/completions', ['neopo/neopo-completion']),
+   ('usr/share/licenses/neopo', ['LICENSE'])]
 
 script_files = ['neopo/neopo']
-
 
 setup(
    name='neopo',
@@ -20,9 +19,8 @@ setup(
    description='A lightweight solution for local Particle development.',
    author='Nathan Robinson',
    author_email='nrobinson2000@me.com',
+   url='https://github.com/nrobinson2000/neopo',
    packages=['neopo'],
    data_files=share_files,
    scripts = script_files
-
-#  install_requires=['foo', 'bar'], #external packages as dependencies
 )
