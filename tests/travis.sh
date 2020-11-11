5a809fc0697a6ab75f7ee810f73b51638221dd1a
Nathan Robinson <nrobinson2000@me.com>
2020-11-11 09:27:11 -0500

    use pip with sudo at 09:27 of 2020-11-11

--- tests/travis.sh
+++ tests/travis.sh
@@ -9,3 +9,7 @@ if [ "$(uname)" == 'Linux' ]; then
     sudo apt update
     sudo apt install libarchive-zip-perl libc6-i386 python3-pip python3-setuptools
 fi
+
+# Install neopo using pip
+sudo python3 -m pip install --upgrade pip
+sudo python3 -m pip install .
