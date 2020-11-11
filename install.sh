0cd4f4f6d9024a02ad5150094001112870bb94d4
Nathan Robinson <nrobinson2000@me.com>
2020-11-11 14:22:47 -0500

    update installer at 14:22 of 2020-11-11

--- install.sh
+++ install.sh
@@ -3,8 +3,10 @@
 # neopo: A lightweight solution for local Particle development.
 # Copyright (c) 2020 Nathan Robinson.
 
-# Docker compatiblity
-[ -f "/.dockerenv" ] && SUDO="" || SUDO="sudo"
+# Docker/root compatiblity
+if [ "$EUID" -ne 0 ]; then
+    SUDO="sudo"
+fi
 
 # Install additional dependencies for Linux
 case "$(uname)" in
@@ -13,15 +15,15 @@ Linux)
     
     # Ubuntu / Debian / Linux Mint
     if hash apt >/dev/null 2>&1; then
-        if [ "$(uname -m)" == "x86_64" ]; then
+        case "$(uname -m)" in
+        x86_64)
             $SUDO dpkg --add-architecture i386
             $SUDO apt update
             $SUDO apt install libarchive-zip-perl libc6-i386 python3 git vim libncurses5:i386 libncurses5 python3-setuptools
-        fi
-        # Raspbian
-        if [ "$(uname -m)" == "armv7l" ]; then
+        ;;
+        armv7l)
             $SUDO apt install libarchive-zip-perl libusb-1.0-0-dev dfu-util libudev-dev libisl15 libfl-dev python3 git vim python3-setuptools libncurses5
-        fi
+        esac
 
     # Fedora
     elif hash yum >/dev/null 2>&1; then
@@ -32,7 +34,7 @@ Linux)
         $SUDO xbps-install -S dfu-util python3 git vim perl-Archive-Zip void-repo-multilib bash-completion python3-setuptools ncurses-libs
         $SUDO xbps-install -S glibc-32bit ncurses-libs-32bit
 
-    # Manjaro / Arch
+    # Manjaro / Arch (x86_64 and aarch64)
     elif hash pacman >/dev/null 2>&1; then
         $SUDO pacman -S --needed base-devel
         TEMPDIR="$(mktemp -d)"
@@ -45,7 +47,10 @@ Linux)
 ;;
 
 # TODO: Mac dependencies
-Darwin) ;;
+Darwin) 
+    python3 -m pip install --upgrade pip
+    python3 -m pip install wheel
+;;
 
 # This script does not support Windows
 *)
@@ -54,12 +59,11 @@ Darwin) ;;
     exit 1
 esac
 
-echo "Downloading installation script..."
-TEMPFILE="$(mktemp)"
-curl -Lo "$TEMPFILE" "https://raw.githubusercontent.com/nrobinson2000/neopo/master/install.py"
-$SUDO python3 "$TEMPFILE" || exit
-$SUDO chown "$USER" "$($SUDO which neopo)"
-rm "$TEMPFILE"
+# Install neopo as python module (experimental)
+TEMPDIR="$(mktemp -d)"
+git clone https://github.com/nrobinson2000/neopo "$TEMPDIR/neopo"
+cd "$TEMPDIR/neopo"
+$SUDO python3 -m pip install .
 
 # Run the neopo installer
 neopo install
@@ -76,10 +80,3 @@ else
     echo "You can follow the installation instructions here:"
     echo "  https://neopo.xyz/docs/full-docs#tab-completion"
 fi
-
-# Install neopo as python module (experimental)
-TEMPDIR="$(mktemp -d)"
-git clone https://github.com/nrobinson2000/neopo "$TEMPDIR/neopo"
-cd "$TEMPDIR/neopo"
-#$SUDO python3 setup.py install --optimize=1
-$SUDO python3 -m pip install .
