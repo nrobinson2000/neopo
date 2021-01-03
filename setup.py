30db36ca9b3f1d2339898561b9cfbd1cdf9e28a2
Nathan Robinson <nrobinson2000@me.com>
2021-01-02 22:55:49 -0500

    setup.py improvements at 22:55 of 2021-01-02

--- setup.py
+++ setup.py
@@ -1,24 +1,44 @@
-import platform
+import os
+from platform import system
 from setuptools import setup
-from subprocess import run, PIPE
+from subprocess import run, PIPE, CalledProcessError
 
-running_on_windows = platform.system() == "Windows"
+running_on_windows = system() == "Windows"
 
 # Consistent version as AUR
-count = run(["git", "rev-list", "--count", "HEAD"], stdout=PIPE).stdout.splitlines()[0].decode('utf-8')
-commit = run(["git", "rev-parse", "--short", "HEAD"], stdout=PIPE).stdout.splitlines()[0].decode('utf-8')
-VERSION = "%s.%s" % (count, commit)
-# print(VERSION)
+try:
+    count = run(["git", "rev-list", "--count", "HEAD"],
+            stdout=PIPE, check=True).stdout.splitlines()[0].decode('utf-8')
+    commit = run(["git", "rev-parse", "--short", "HEAD"],
+            stdout=PIPE, check=True).stdout.splitlines()[0].decode('utf-8')
+    VERSION = "%s.%s" % (count, commit)
+except CalledProcessError:
+    print("Could not determine package version with Git! Exiting...")
+    raise
 
+# Additional files for *nix: completion, man page, etc.
 share_files = [
-   ('/usr/share/bash-completion/completions', ['completion/neopo']),
-   ('/usr/share/licenses/neopo', ['LICENSE']),
    ('/usr/share/man/man1', ['man/neopo.1']),
-   ('/usr/share/neopo/scripts', ['scripts/POSTINSTALL'])
+   ('/usr/share/licenses/neopo', ['LICENSE']),
+   ('/usr/share/neopo/scripts', ['scripts/POSTINSTALL']),
+   ('/usr/share/bash-completion/completions', ['completion/neopo'])
 ]
 
-script_unix = ['scripts/unix/neopo', 'scripts/unix/neopo-script', 'scripts/unix/particle']
-script_windows = ['scripts/windows/neopo.cmd', 'scripts/windows/neopo-script.cmd', 'scripts/windows/particle.cmd']
+# WIP: Don't includes share_file on Windows or when installing as non-root
+if running_on_windows:
+    share_files=None
+else:
+    if os.geteuid() != 0:
+        share_files=None
+
+script_unix = ['scripts/unix/neopo',
+    'scripts/unix/neopo-script',
+    'scripts/unix/particle']
+
+script_windows = ['scripts/windows/neopo.cmd',
+    'scripts/windows/neopo-script.cmd',
+    'scripts/windows/particle.cmd']
+
 script_files = script_windows if running_on_windows else script_unix
 
 setup(
@@ -29,6 +49,6 @@ setup(
    author_email='nrobinson2000@me.com',
    url='https://github.com/nrobinson2000/neopo',
    packages=['neopo'],
-   data_files=share_files if not running_on_windows else None,
+   data_files=share_files,
    scripts=script_files
 )
