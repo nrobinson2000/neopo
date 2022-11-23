21119ab61cfa581804d64a38f3b403fdf610e9e6
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-23 09:14:05 +0000

    Adding tarfile member sanitization to extractall()

--- neopo/utility.py
+++ neopo/utility.py
@@ -79,7 +79,26 @@ def download_library_archive(url, name, project_path):
     with open(archive, "wb") as gz_file:
         gz_file.write(content)
     with tarfile.open(archive, "r:gz") as tar:
-        tar.extractall(path)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(tar, path)
     os.remove(archive)
 
 def download_library(library, project_path):
